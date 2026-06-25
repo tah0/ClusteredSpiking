1062ef94ac455a71ec4abd785aee9f8c099c34f0
Todd Hagen <todd.a.hagen@gmail.com>
2026-06-25 11:59:04 +0200

    Fix package structure: module in ClusteredSpiking.jl, bare functions in csn_ctln.jl

--- src/ClusteredSpiking.jl
+++ src/ClusteredSpiking.jl
@@ -1,1 +1,29 @@
+module ClusteredSpiking
+
+using DifferentialEquations
+using LinearAlgebra
+using Plots
+using Graphs
+using GraphPlot
+using GraphRecipes
+using Colors
+using Cairo
+using Fontconfig
+using Plots.PlotMeasures
+using StatsBase
+using ProgressMeter
+using Combinatorics
+using DataFrames
+using StatsPlots
+
+export tln_function, solve_TLN, tln_function_inhib, solve_TLN_inhib,
+       write_sa, graph_to_weights, graph_to_weights_inhib,
+       plot_sol, n_digits, make_names, running_average,
+       graph_plot, graph_to_plot, random_from_sA, plot_sol_inhib,
+       sim_tln_spiking, sim_v_model_spiking,
+       pop_rates, mean_by_pop, fixpts,
+       W_sig, b_sig, switch_times, v_to_r, r_to_v, filter_input
+
 include("csn_ctln.jl")
+
+end # module ClusteredSpiking
--- src/csn_ctln.jl
+++ src/csn_ctln.jl
@@ -1,28 +1,3 @@
-module ClusteredSpiking
-
-using DifferentialEquations
-using LinearAlgebra
-using Plots
-using Graphs
-using GraphPlot
-using GraphRecipes
-using Colors
-using Cairo
-using Fontconfig
-using Plots.PlotMeasures
-using StatsBase
-using ProgressMeter
-using Combinatorics
-using DataFrames
-using StatsPlots
-
-export tln_function, solve_TLN, tln_function_inhib, solve_TLN_inhib,
-       write_sa, graph_to_weights, graph_to_weights_inhib,
-       plot_sol, n_digits, make_names, running_average,
-       graph_plot, graph_to_plot, random_from_sA, plot_sol_inhib,
-       sim_tln_spiking, sim_v_model_spiking,
-       pop_rates, mean_by_pop, fixpts,
-       W_sig, b_sig, switch_times, v_to_r, r_to_v, filter_input
 
 function tln_function(W, b; tau = 1,  stimfunction)
     function update!(du, u, p, t)
@@ -518,5 +493,3 @@ function filter_input(tau, b, s, T)
     end
     return result
 end
-
-end # module ClusteredSpiking
